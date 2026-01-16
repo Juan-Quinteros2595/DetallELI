03ceb01f4936e9e5d268c121343b847600c8288c
Disin Juan <juanquinteros0404@gmail.com>
2026-01-16 06:08:40 -0300

    Fix: renamed images and removed node_modules from tracking

--- components/sections/section-6-layered.tsx
+++ components/sections/section-6-layered.tsx
@@ -7,19 +7,19 @@ export default function Section6Layered() {
       {/* BACKGROUND IMAGES (corners) */}
       <div className="absolute inset-0 pointer-events-none">
         <img
-          src="/Grupo6/bad-bunny1.jpg"
+          src="/grupo-6/bad-bunny1.jpg"
           className="absolute top-0 left-0 w-1/2 h-1/2 object-cover opacity-40"
         />
         <img
-          src="/Grupo6/bad-bunny2.jpg"
+          src="/grupo-6/bad-bunny2.jpg"
           className="absolute top-0 right-0 w-1/2 h-1/2 object-cover opacity-40"
         />
         <img
-          src="/Grupo6/bad-bunny3.jpg"
+          src="/grupo-6/bad-bunny3.jpg"
           className="absolute bottom-0 left-0 w-1/2 h-1/2 object-cover opacity-40"
         />
         <img
-          src="/Grupo6/bad-bunny4.jpg"
+          src="/grupo-6/bad-bunny4.jpg"
           className="absolute bottom-0 right-0 w-1/2 h-1/2 object-cover opacity-40"
         />
       </div>
@@ -28,22 +28,22 @@ export default function Section6Layered() {
         <div className="relative w-full max-w-5xl h-[70%]">
           
           <img
-            src="/Grupo6/badbunny.jpg"
+            src="/grupo-6/bad-bunny.jpg"
             className="animate-on-scroll anim-type-5 absolute top-[-50] left-32 w-72 rotate-[-6deg]"
           />
 
           <img
-            src="/Grupo6/bad-bunny.jpg"
+            src="/grupo-6/bad-bunny10.jpg"
             className="animate-on-scroll anim-type-5 delay-1 absolute top-0 right-24 w-72 rotate-[6deg]"
           />
 
           <img
-            src="/Grupo6/bad-bunny7.jpg"
+            src="/grupo-6/bad-bunny7.jpg"
             className="animate-on-scroll anim-type-5 delay-2 absolute bottom-0 left-5 w-72 rotate-[7deg]"
           />
 
           <img
-            src="/Grupo6/bad-bunny8.jpg"
+            src="/grupo-6/bad-bunny8.jpg"
             className="animate-on-scroll anim-type-5 delay-3 absolute bottom-0 right-30 w-72 rotate-[-4deg]"
           />
         </div>
