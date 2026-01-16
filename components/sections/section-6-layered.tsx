8abcd9d44f97708b9740d1bc5603033e4cad6824
Disin Juan <juanquinteros0404@gmail.com>
2026-01-16 05:52:22 -0300

    fix: filenames and paths

--- components/sections/section-6-layered.tsx
+++ components/sections/section-6-layered.tsx
@@ -7,19 +7,19 @@ export default function Section6Layered() {
       {/* BACKGROUND IMAGES (corners) */}
       <div className="absolute inset-0 pointer-events-none">
         <img
-          src="/Grupo6/Bad_Bunny1.jpg"
+          src="/Grupo6/bad-bunny1.jpg"
           className="absolute top-0 left-0 w-1/2 h-1/2 object-cover opacity-40"
         />
         <img
-          src="/Grupo6/Bad_Bunny2.jpg"
+          src="/Grupo6/bad-bunny2.jpg"
           className="absolute top-0 right-0 w-1/2 h-1/2 object-cover opacity-40"
         />
         <img
-          src="/Grupo6/Bad_Bunny3.jpg"
+          src="/Grupo6/bad-bunny3.jpg"
           className="absolute bottom-0 left-0 w-1/2 h-1/2 object-cover opacity-40"
         />
         <img
-          src="/Grupo6/Bad_Bunny4.jpg"
+          src="/Grupo6/bad-bunny4.jpg"
           className="absolute bottom-0 right-0 w-1/2 h-1/2 object-cover opacity-40"
         />
       </div>
@@ -33,17 +33,17 @@ export default function Section6Layered() {
           />
 
           <img
-            src="/Grupo6/Bad_Bunny10.jpg"
+            src="/Grupo6/bad-bunny.jpg"
             className="animate-on-scroll anim-type-5 delay-1 absolute top-0 right-24 w-72 rotate-[6deg]"
           />
 
           <img
-            src="/Grupo6/Bad_Bunny7.jpg"
+            src="/Grupo6/bad-bunny7.jpg"
             className="animate-on-scroll anim-type-5 delay-2 absolute bottom-0 left-5 w-72 rotate-[7deg]"
           />
 
           <img
-            src="/Grupo6/Bad_Bunny8.jpg"
+            src="/Grupo6/bad-bunny8.jpg"
             className="animate-on-scroll anim-type-5 delay-3 absolute bottom-0 right-30 w-72 rotate-[-4deg]"
           />
         </div>
